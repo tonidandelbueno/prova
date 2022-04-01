dba72a1343a6de0c5b70766837d8ba7c7cd5da2c
tonidandelbueno <101369997+tonidandelbueno@users.noreply.github.com>
2022-03-31 21:16:58 -0300

    Add files via upload

---
+++ exe.py
@@ -0,0 +1,16 @@
+#lista 4 atividade 2
+
+#a)
+for n in range(10):
+    print(n)
+
+    print('-------------')
+
+for n in range(-6,-6+10):
+    print(n)
+
+print('-------------')
+
+for n in range(0,20,2):
+    print(n)
+
